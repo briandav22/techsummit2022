d60a477a210a097bec4d2fb744e52fb635c4f68b
Brian Davenport <briandavenport@Brians-MacBook-Pro.local>
2022-04-12 14:42:06 -0400

    more

--- simple_dash_app/app.py
+++ simple_dash_app/app.py
@@ -1,37 +1,19 @@
-import plotly.graph_objects as go # or plotly.express as px
-fig = go.Figure() # or any Plotly Express function e.g. px.bar(...)
-# fig.add_trace( ... )
-# fig.update_layout( ... )
-
-from dash import dcc,html, dash, Input,Output
-
+from dash import dcc,html, dash
 import pandas as pd
-
-from fig_maker import make_fig
+import plotly.express as px
 
 df = pd.read_csv('data.csv')
 
-
-
-
 app = dash.Dash()
 
-fig = make_fig(df,'Line Graph')
+fig = px.line(df, x="name", y="average_utilization", title='Line Graph')
 
 app.layout = html.Div([
-    dcc.Graph(id='example_fig',figure=fig),
-    dcc.Dropdown(['Pie Chart', 'Line Graph', 'Bar Chart'], 'Line Graph', id='demo-dropdown'),
+    html.Div('Simple Dash App for Tech Summit 2022'),
+    dcc.Graph(figure=fig),
+    html.Div('Line Graph showing Average Utilization'),
+
 ])
-
-
-@app.callback(
-    Output('example_fig','figure'),
-    Input('demo-dropdown','value')
-    )
-
-def swap_figure(graph_type):
-    fig = make_fig(df,graph_type)
-    return fig
 
 
 if __name__ == '__main__':
